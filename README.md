4f22649a3fc1cce904a09b7ef2c896b366d5df0a
Maximilian Crosby <maximilian@maximilians-mbp.home>
2019-06-18 21:50:02 +0100

    ErrorBoundaries & README update

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Need to learn / Goals:
 - [X] SCSS.
 - [X] Bootstrap.
 - [ ] React.
+
+React Learning Points:
+
+- [X] Primitive and Reference data types.
+- [X] Complete SC6 and SC7 syntactic understanding.
+- [X] Create-React-App.
+- [X] Common packages used with React. IE. Babel, WebPack.
+- [X] A comprehensive understanding of JSX.
+- [X] Statefull and Stateless, Functional and Class based Components.
+- [X] Working with inline styles and Radium.
+- [X] Conditional rendering.
+- [X] Outputting lists / DNR.
+- [X] CSS Modules and Dynamic styles.
+- [X] Using Dev Tools with React.
+- [X] Implementing and using errorBoundaries.
--- react/main-app-ejected/src/Person/Person.js
+++ react/main-app-ejected/src/Person/Person.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './scss/person.css'
+// with CRA ejected and CSS Modules: true, we can now name import our CSS stylesheet
+// and access each class with dot syntax.
+import styles from './scss/person.css'
 
 // This is a stateless component. Notice no 'useState' or 'setState'.
 const person = props => {
@@ -24,7 +26,7 @@ const person = props => {
   };
 
   return (
-    <div className="person">
+    <div className={styles.person}>
       <input type="text" onChange={props.change} value={isGay(props.name)}/>
       <h4 onClick={props.delete}>I am {isGay(props.name)} and I am {props.age} {multiple(props.children)}</h4>
     </div>
