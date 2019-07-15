59f14250adac4d544cb692691eb47d22972cf321
Maximilian Crosby <maximilian@maximilians-mbp.home>
2019-07-15 18:57:48 +0100

    Burger-Builder with FireBase

--- react/burger-builder/src/components/Nav/Toolbar/Toolbar.js
+++ react/burger-builder/src/components/Nav/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import classes from './scss/Toolbar.module.css'
 import WithClass from '../../../hoc/WithClass'
@@ -7,22 +7,21 @@ import NavItems from '../NavItems/NavItems'
 import MenuLink from '../NavItems/MenuLink/MenuLink'
 import NavLink from '../NavItems/NavLink/NavLink'
 
-const Toolbar = props => (
-  <header className={classes.Toolbar}>
-    <WithClass classes={classes.Flex}>
-      <WithClass classes={classes.Logo}>
-        <Logo />
+const Toolbar = props => 
+    <header className={classes.Toolbar}>
+      <WithClass classes={classes.Flex}>
+        <WithClass classes={classes.Logo}>
+          <Logo />
+        </WithClass>
+        {props.userEmail ? <div className={classes.Username}><NavLink link='/auth'>{props.userEmail}</NavLink></div> : null}
       </WithClass>
-      {props.userEmail ? <div className={classes.Username}><NavLink link='/auth'>{props.userEmail}</NavLink></div> : null}
-    </WithClass>
-    <nav className={classes.Desktop}>
-      <NavItems />
-    </nav>
-    <WithClass classes={classes.MenuLinkHide}>
-      <MenuLink click={props.open}>Menu</MenuLink>
-    </WithClass>
-  </header>
-)
+      <nav className={classes.Desktop}>
+        <NavItems />
+      </nav>
+      <WithClass classes={classes.MenuLinkHide}>
+        <MenuLink click={props.open}>Menu</MenuLink>
+      </WithClass>
+    </header>
 
 const mapStateToProps = state => {
   return {
