ac63f9dcccd628766adbe5173af72a6623093dec
Maximilian118 <maxcrosby118@gmail.com>
2022-06-18 17:25:12 -0400

    ts

---
+++ ts/start/app.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+var add = function (n1, n2) { return n1 + n2; };
+var num1 = '5';
+var num2 = 2.8;
+var aBoolean = true;
+var res = add(num1, num2, aBoolean);
+console.log(res);
---
+++ ts/start/app.ts
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+const add = (n1: number, n2: number) => n1 + n2
+
+const num1 = '5'
+const num2 = 2.8
+const aBoolean = true
+
+const res = add(num1, num2, aBoolean)
+console.log(res)
