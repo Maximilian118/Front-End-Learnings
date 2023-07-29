45a91df56db5d5428d9210997a4cbcc93d0725cf
Maximilian118 <maxcrosby118@gmail.com>
2023-07-29 15:32:41 +0200

    Typeguards

--- ts/advanced-types/dist/app.js
+++ ts/advanced-types/dist/app.js
@@ -5,4 +5,20 @@ const emp1 = {
     startDate: new Date(),
 };
 console.log(emp1);
+const add = (a, b) => {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+};
+const employeeInfo = (emp) => {
+    console.log(`name: ${emp.name}`);
+    if ("permissions" in emp) {
+        console.log(`permissions: ${emp.permissions}`);
+    }
+    if ("startDate" in emp) {
+        console.log(`start date: ${emp.startDate}`);
+    }
+};
+employeeInfo(emp1);
 //# sourceMappingURL=app.js.map
--- ts/advanced-types/src/app.ts
+++ ts/advanced-types/src/app.ts
@@ -18,3 +18,34 @@ const emp1: ElevatedEmployee = {
 }
 
 console.log(emp1)
+
+type Combine = string | number
+type Numeric = number | boolean
+type Universal = Combine & Numeric
+
+const add = (a: Universal, b: Universal) => {
+  if (typeof a === "string" || typeof b === "string") {
+    // This is a typeguard.
+    return a.toString() + b.toString()
+  }
+  return a + b
+}
+
+type UnknownEmployee = Admin | Employee
+
+const employeeInfo = (emp: UnknownEmployee) => {
+  // The problem here is that we don't know if emp.permissions is available.
+  // Also, typeof can't help is as we can only check for types that JS as aware of.
+  console.log(`name: ${emp.name}`)
+  if ("permissions" in emp) {
+    console.log(`permissions: ${emp.permissions}`)
+  }
+  if ("startDate" in emp) {
+    console.log(`start date: ${emp.startDate}`)
+  }
+}
+
+employeeInfo(emp1)
+
+// instanceof can be used instead of in for classes!
+// for example: vehicle instanceof Truck
