81d83fee9ed9f273eb52bf853a67ca029e7fc578
Maximilian118 <maxcrosby118@gmail.com>
2024-03-25 09:38:56 +1100

    The declare keyword

--- ts/drag-and-drop-project/src/app.ts
+++ ts/drag-and-drop-project/src/app.ts
@@ -2,8 +2,11 @@ import { ProjectList } from "./projects/projects"
 import { ProjectInput } from "./projects/form"
 import _ from "lodash"
 
+declare var GLOBAL: string
+
 new ProjectInput()
 new ProjectList("active")
 new ProjectList("done")
 
 console.log(_.shuffle([1, 2, 3]))
+console.log(GLOBAL)
