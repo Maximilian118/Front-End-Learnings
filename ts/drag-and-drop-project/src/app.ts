504b47df275931d580c472c535a30ca9ee283621
Maximilian118 <maxcrosby118@gmail.com>
2024-03-25 09:30:31 +1100

    Type definition packages

--- ts/drag-and-drop-project/src/app.ts
+++ ts/drag-and-drop-project/src/app.ts
@@ -1,6 +1,9 @@
 import { ProjectList } from "./projects/projects"
 import { ProjectInput } from "./projects/form"
+import _ from "lodash"
 
 new ProjectInput()
 new ProjectList("active")
 new ProjectList("done")
+
+console.log(_.shuffle([1, 2, 3]))
