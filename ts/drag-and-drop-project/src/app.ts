716d5ec0ce74cfe8b20c33028d935c4a1dc04fde
Maximilian118 <maxcrosby118@gmail.com>
2024-03-27 13:33:16 +0000

    Using a TS Module: class-validator with decorators

--- ts/drag-and-drop-project/src/app.ts
+++ ts/drag-and-drop-project/src/app.ts
@@ -1,8 +1,21 @@
+import { Product } from "./product.model"
+import { validate } from "class-validator"
 import { ProjectList } from "./projects/projects"
 import { ProjectInput } from "./projects/form"
 import _ from "lodash"
 
 declare var GLOBAL: string
+
+const newProd = new Product("", -5.99)
+
+validate(newProd).then((err) => {
+  if (err.length > 0) {
+    console.log("Wups!! Errors:")
+    console.log(err)
+  } else {
+    console.log(newProd.getInformation())
+  }
+})
 
 new ProjectInput()
 new ProjectList("active")
